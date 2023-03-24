8f9ba7e07a744b0a3d9449dfd987d270ee9875bb
Dar Ginelsa <darriel.ginelsa@pandarg.com>
2023-03-23 19:49:15 -0700

    Add mini me confirm and builder pages

---
+++ src/app/mini-me/mini-me-builder/mini-me-builder.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-me-builder',
+  templateUrl: './mini-me-builder.page.html',
+  styleUrls: ['./mini-me-builder.page.scss'],
+})
+export class MiniMeBuilderPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/mini-me/mini-me-confirm/mini-me-confirm.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-me-confirm',
+  templateUrl: './mini-me-confirm.page.html',
+  styleUrls: ['./mini-me-confirm.page.scss'],
+})
+export class MiniMeConfirmPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
