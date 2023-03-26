774039b937c746e7b6fc1e39ad4f9fd71fe15aff
Kaisen L <kaisen@uxwizards.org>
2023-03-25 20:14:40 -0700

    Add GA4 test event and use history route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, useHash: true })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, useHash: false })
   ],
   exports: [RouterModule]
 })
