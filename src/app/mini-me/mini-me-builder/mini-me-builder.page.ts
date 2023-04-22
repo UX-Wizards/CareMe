d414a7e4f9760e6c4c8c2e7201f376aae668479f
Dar Ginelsa <darriel.ginelsa@pandarg.com>
2023-04-21 20:31:10 -0700

    Created Mini Me builder functionality

--- src/app/mini-me/mini-me-builder/mini-me-builder.page.ts
+++ src/app/mini-me/mini-me-builder/mini-me-builder.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild } from '@angular/core';
+import { SwiperComponent } from "swiper/angular";
+import { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-mini-me-builder',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mini-me-builder.page.scss'],
 })
 export class MiniMeBuilderPage implements OnInit {
+  @ViewChild('builder') swiperBuilder?: SwiperComponent
+  @ViewChild('selections') swiperSelection?: SwiperComponent
+  @Output() nextSlide = new EventEmitter<any>()
+  menuConfig: SwiperOptions = {
+    slidesPerView: 4,
+    centeredSlides: true
+  }
+  selectionConfig: SwiperOptions = {
+    pagination: false,
+    allowTouchMove: false
+  }
+  swiper: any
+  currentIndex?: number
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSwiper(swiper: any) {
+    this.currentIndex = swiper.realIndex
+  }
+
+  slideNext() {
+    this.nextSlide.emit()
+  }
+
+  slideTo(i: number) {
+    this.swiperBuilder?.swiperRef.slideTo(i)
+    this.currentIndex = this.swiperBuilder?.swiperRef.activeIndex
+  }
+
+  onActiveIndexChange() {
+    this.currentIndex = this.swiperBuilder?.swiperRef.activeIndex
+    this.slideToSelection(this.swiperBuilder?.swiperRef.activeIndex)
+  }
+
+  slideToSelection(i: any) {
+    this.swiperSelection?.swiperRef.slideTo(i);
+    this.currentIndex = this.swiperBuilder?.swiperRef.activeIndex
   }
 
 }
--- src/app/mini-me/mini-me-setup/mini-me-setup.page.ts
+++ src/app/mini-me/mini-me-setup/mini-me-setup.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-mini-me-setup',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mini-me-setup.page.scss'],
 })
 export class MiniMeSetupPage implements OnInit {
-
+  @Output() nextSlide = new EventEmitter<any>()
   constructor() { }
 
   ngOnInit() {
+  }
+
+  slideNext() {
+    this.nextSlide.emit()
   }
 
 }
