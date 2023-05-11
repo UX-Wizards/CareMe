1237b8791b4ca91359174c7113d5d68f9add8caf
Dar Ginelsa <darriel.ginelsa@pandarg.com>
2023-05-11 16:50:33 -0700

    Added skin color functionality. Removed scrolling up and down when setting up Mini Me.

--- src/app/mini-me/mini-me-builder/mini-me-builder.page.ts
+++ src/app/mini-me/mini-me-builder/mini-me-builder.page.ts
@@ -20,10 +20,39 @@ export class MiniMeBuilderPage implements OnInit {
   }
   swiper: any
   currentIndex?: number
+  avatar: any = {
+    topType: "LongHairStraightStrand",
+    accessoriesType: "Blank",
+    hairColor: "Black",
+    facialHairType: "Blank",
+    clotheType: "Hoodie",
+    clotheColor: "Blue03",
+    eyeType: "Happy",
+    eyebrowType: "Default",
+    mouthType: "Twinkle",
+    skinColor: "Pale"
+  }
+  image: string = `https://avataaars.io/?avatarStyle=Circle&topType=${this.avatar.topType}&accessoriesType=Blank&hairColor=${this.avatar.hairColor}&facialHairType=Blank&clotheType=${this.avatar.clotheType}&clotheColor=${this.avatar.clotheColor}&eyeType=${this.avatar.eyeType}&eyebrowType=${this.avatar.eyebrowType}&mouthType=${this.avatar.mouthType}&skinColor=${this.avatar.skinColor}`
+  animate: boolean = false
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  generateAvatar() {
+    this.image = `https://avataaars.io/?avatarStyle=Circle&topType=${this.avatar.topType}&accessoriesType=Blank&hairColor=${this.avatar.hairColor}&facialHairType=Blank&clotheType=${this.avatar.clotheType}&clotheColor=${this.avatar.clotheColor}&eyeType=${this.avatar.eyeType}&eyebrowType=${this.avatar.eyebrowType}&mouthType=${this.avatar.mouthType}&skinColor=${this.avatar.skinColor}`
+    // Animation hack
+    if (this.animate) {
+      this.animate = false
+    } else {
+      this.animate = true
+    }
+  }
+
+  selectSkinColor(color: string) {
+    this.avatar.skinColor = color
+    this.generateAvatar()
   }
 
   onSwiper(swiper: any) {
