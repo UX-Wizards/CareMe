e322e822d0c1f2d78d2bc3078ce1f8d7bc317dcc
Dar Ginelsa <darriel.ginelsa@pandarg.com>
2023-05-17 20:41:09 -0700

    Completed Mini Me builder

--- src/app/mini-me/mini-me-builder/mini-me-builder.page.ts
+++ src/app/mini-me/mini-me-builder/mini-me-builder.page.ts
@@ -11,6 +11,7 @@ export class MiniMeBuilderPage implements OnInit {
   @ViewChild('menu') swiperMenu?: SwiperComponent
   @ViewChild('selections') swiperSelection?: SwiperComponent
   @Output() nextSlide = new EventEmitter<any>()
+  @Output() saveAvatar = new EventEmitter<any>()
   menuConfig: SwiperOptions = {
     slidesPerView: 4,
     centeredSlides: true
@@ -34,6 +35,7 @@ export class MiniMeBuilderPage implements OnInit {
   }
   image: string = `https://avataaars.io/?avatarStyle=Circle&topType=${this.avatar.topType}&accessoriesType=Blank&hairColor=${this.avatar.hairColor}&facialHairType=Blank&clotheType=${this.avatar.clotheType}&clotheColor=${this.avatar.clotheColor}&eyeType=${this.avatar.eyeType}&eyebrowType=${this.avatar.eyebrowType}&mouthType=${this.avatar.mouthType}&skinColor=${this.avatar.skinColor}`
   animate: boolean = false
+  showClotheColor: boolean = true
 
   constructor() { }
 
@@ -48,10 +50,52 @@ export class MiniMeBuilderPage implements OnInit {
     } else {
       this.animate = true
     }
+    //Show clothe color
+    if (this.avatar.clotheType === 'BlazerShirt' || this.avatar.clotheType === 'BlazerSweater') {
+      this.showClotheColor = false
+    } else {
+      this.showClotheColor = true
+    }
+    this.saveAvatar.emit(this.image)
   }
 
   selectSkinColor(color: string) {
     this.avatar.skinColor = color
+    this.generateAvatar()
+  }
+
+  selectTopType(type: string) {
+    this.avatar.topType = type
+    this.generateAvatar()
+  }
+
+  selectHairColor(color: string) {
+    this.avatar.hairColor = color
+    this.generateAvatar()
+  }
+
+  selectEyeType(type: string) {
+    this.avatar.eyeType = type
+    this.generateAvatar()
+  }
+
+  selectEyebrowType(type: string) {
+    this.avatar.eyebrowType = type
+    this.generateAvatar()
+  }
+
+  selectMouthType(type: string) {
+    this.avatar.mouthType = type
+    this.generateAvatar()
+  }
+
+  selectClotheType(type: string) {
+    this.avatar.clotheType = type
+    this.generateAvatar()
+  }
+
+  selectClotheColor(color: string) {
+    this.avatar.clotheColor = color
     this.generateAvatar()
   }
 
